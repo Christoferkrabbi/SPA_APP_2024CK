8da18e60d5ad189590adecefc78eeed0c8f57b84
christoferkrabbi <christoferkrabbi@gmail.com>
2025-03-28 11:36:24 +0200

    lkm

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,33 +1,12 @@
 <template>
-
+    <EventList :title="title" />
+    <PeopleList :title="title" />
 </template>
 
 <script setup lang="ts">
     import EventList from "@/components/EventList.vue";
     import PeopleList from "@/components/PeopleList.vue";
     import { defineProps } from "vue";
-    import axios from "axios";
-
-    // Define props passed to the component
-    defineProps<{ title: string }>();
-
-    // Function to create a new event
-    const CreateEvent = async () => {
-        try {
-            const response = await axios.post('/Controllers/EventsController-CreateEvent');
-            alert('Sündmus lisatud: ' + response.data);
-        } catch (error) {
-            console.error('Viga:', error);
-        }
-    };
-
-    // Function to create a new person
-    const CreatePerson = async () => {
-        try {
-            const response = await axios.post('/Controllers/PeopleController-CreatePerson');
-            alert('Isik lisatud: ' + response.data);
-        } catch (error) {
-            console.error('Viga:', error);
-        }
-    };
+    
+    define<{ title: String }>();
 </script>
